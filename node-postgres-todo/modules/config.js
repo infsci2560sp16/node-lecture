da40f6f1d0ad352a133c03fea881501c5e5e136d
Brian J Kolowitz <bkolowitz@gmail.com>
2016-04-13 22:41:14 -0400

    add todo example

---
+++ node-postgres-todo/modules/config.js
@@ -0,0 +1,4 @@
+function config() {
+    return 'postgres://todo_user:Password1@localhost:5432/todo';
+}
+exports.config = config;
---
+++ node-postgres-todo/modules/database.js
@@ -0,0 +1,8 @@
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://todo_user:Password1@localhost:5432/todo';
+
+var client = new pg.Client(connectionString);
+client.connect();
+
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', function() { client.end(); });
